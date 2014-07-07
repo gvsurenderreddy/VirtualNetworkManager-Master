924985b6b79de1757566a2a111917b6752ae0877
swarvanusg <swarvanu007@gmail.com>
2014-07-07 12:17:35 +0530

    "Adding VirtualNeworkManager"

---
+++ virtualNetworkManager/src/test/java/org/opendaylight/controller/tutorial_L2_forwarding/internal/TutorialL2ForwardingTest.java
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.tutorial_L2_forwarding.internal;
+
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.controller.virtualNetworkManager.VirtualNetworkManager;
+
+public class TutorialL2ForwardingTest extends TestCase {
+
+        @Test
+        public void testTutorialL2ForwardingCreation() {
+
+                VirtualNetworkManager ah = null;
+                ah = new VirtualNetworkManager();
+                Assert.assertTrue(ah != null);
+
+        }
+
+}
